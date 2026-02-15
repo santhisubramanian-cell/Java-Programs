4a70b6b9d972b4f5a4ce87ce170f205c8d90fbab
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:59:18 +0530

    Add files via upload
    College Event Fund Management

---
+++ Basic18.java
@@ -0,0 +1,18 @@
+package javaprograms;
+import java.util.Scanner;
+public class Basic18 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int registration = scan.nextInt();
+		int sponsor = scan.nextInt();
+		int stall = scan.nextInt();
+		int stage = scan.nextInt();
+		int celebrity = scan.nextInt();
+		int marketing = scan.nextInt();
+		int a = registration +sponsor+stall-stage-celebrity-marketing;
+		System.out.println(a);
+		
+	}
+
+}
