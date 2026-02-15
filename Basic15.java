03a76d9cd708e988c21a152dfe2db3d09f5e59c6
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:57:36 +0530

    Add files via upload
     Hotel Room Revenue Calculator

---
+++ Basic15.java
@@ -0,0 +1,17 @@
+package javaprograms;
+import java.util.Scanner;
+public class Basic15 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int rooms = scan.nextInt();
+		int price = scan.nextInt();
+		int service = scan.nextInt();
+		int commision = scan.nextInt();
+		int value = (rooms*price)+service-commision;
+		System.out.println(value);
+		
+
+	}
+
+}
