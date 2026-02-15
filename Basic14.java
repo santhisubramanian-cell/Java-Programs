908e92944c82bb13e6e89af614b814f70227dc61
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:57:05 +0530

    Add files via upload
    Gym Trainer Monthly Salary

---
+++ Basic14.java
@@ -0,0 +1,17 @@
+package javaprograms;
+import java.util.Scanner;
+public class Basic14 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int base = scan.nextInt();
+		int session = scan.nextInt();
+		int pay = scan.nextInt();
+		int bonus= scan.nextInt();
+		int mainten= scan.nextInt();	
+		int value = base+(session*pay)+bonus-mainten;
+		System.out.println(value);
+
+	}
+
+}
