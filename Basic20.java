8d03a694d6df1c989dbb787d128e67d1056af147
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 13:00:25 +0530

    Add files via upload
    YouTube Channel Monthly Income

---
+++ Basic20.java
@@ -0,0 +1,16 @@
+package javaprograms;
+import java.util.Scanner;
+public class Basic20 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int ads = scan.nextInt();
+		int sponsor = scan.nextInt();
+		int affiliate = scan.nextInt();
+		int tax = scan.nextInt();
+		int production = scan.nextInt();
+		int value = ads+sponsor+affiliate-tax-production;
+		System.out.println(value);
+	}
+
+}
