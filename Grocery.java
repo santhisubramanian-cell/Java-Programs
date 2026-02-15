c1afc19d034c3ba8ec39b652f5b00b2c7b589b76
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:50:27 +0530

    Add files via upload
    Grocery Store Daily Sales

---
+++ Grocery.java
@@ -0,0 +1,14 @@
+package javaprograms;
+import java.util.Scanner;
+
+public class Grocery {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double sales = scan.nextDouble();
+        double discount = scan.nextDouble();
+        double gst = scan.nextDouble();
+        double finalRevenue = sales - (sales * discount / 100) + (sales * gst / 100);
+        System.out.println("Final Revenue = " + (int)finalRevenue);
+    }
+}
