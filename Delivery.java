6cfaada15483b88067c91b9ab4401c9629c5f7ff
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:55:00 +0530

    Add files via upload
    Delivery Service Earnings Calculator

---
+++ Delivery.java
@@ -0,0 +1,17 @@
+package javaprograms;
+import java.util.Scanner;
+public class Delivery {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int deliveries = scan.nextInt();
+		int payout = scan.nextInt();
+		int incentive = scan.nextInt();
+		int fuelCost = scan.nextInt();
+		int a = (deliveries*payout)+incentive-fuelCost;
+		System.out.println(a);
+		
+
+	}
+
+}
