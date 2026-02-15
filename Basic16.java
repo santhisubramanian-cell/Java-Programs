6e1228b9c16055172b09db0b4d7d3143f1df90e6
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:58:15 +0530

    Add files via upload
    Railway Ticket Revenue System

---
+++ Basic16.java
@@ -0,0 +1,20 @@
+package javaprograms;
+import java.util.Scanner;
+public class Basic16 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int genPass = scan.nextInt();
+		int genfare = scan.nextInt();
+		int acPass = scan.nextInt();
+		int acfare = scan.nextInt();
+		int platform = scan.nextInt();
+		int maintenance = scan.nextInt();
+		int fuel = scan.nextInt();
+		int value = (genPass*genfare)+(acPass*acfare)+platform-maintenance-fuel;
+		System.out.println(value);
+		
+
+	}
+
+}
