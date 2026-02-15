ef22a54450ad3284df38865fdef8bdf0e2d4848d
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:58:46 +0530

    Add files via upload
    Software Company Project Cost Estimator

---
+++ Basic17.java
@@ -0,0 +1,18 @@
+package javaprograms;
+import java.util.Scanner;
+public class Basic17 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int hours = scan.nextInt();
+		int rate = scan.nextInt();
+		int design = scan.nextInt();
+		int testing = scan.nextInt();
+		int advance= scan.nextInt();
+		int a= (hours*rate)+design+testing-advance;
+		System.out.println(a);
+		
+
+	}
+
+}
