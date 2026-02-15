15a619798784ac520d5a8ecdc2ca62ad241cfc37
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-15 12:51:03 +0530

    Add files via upload
     Student Scholarship Eligibility Amount

---
+++ Scholarship.java
@@ -0,0 +1,17 @@
+package javaprograms;
+import java.util.Scanner;
+public class Scholarship {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int tuition = scan.nextInt();
+		int scholar = scan.nextInt();
+		int exam = scan.nextInt();
+		int library = scan.nextInt();
+		int discount = tuition*scholar/100;
+		int pay = tuition-discount+exam+library;
+		System.out.println(pay);
+
+	}
+
+}
