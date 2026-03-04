56f6158e3e0a1f1da5dc9e6bd17502b50632e09e
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-04 21:04:47 +0530

    Add files via upload

---
+++ Nested4.java
@@ -0,0 +1,14 @@
+package nestedloop;
+import java.util.Scanner;
+public class Nested4 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int student = scan.nextInt();
+		int subject = scan.nextInt();
+		
+		
+	}
+
+}
