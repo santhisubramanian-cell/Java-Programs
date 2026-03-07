7d4d4b882ddec12cf26927cc79fed352082866fe
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-07 19:46:01 +0530

    Add files via upload

--- array1.java
+++ array1.java
@@ -1,36 +1,39 @@
-package one_d_array;
+package two_d_array;
 import java.util.Scanner;
 public class array1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int student = scan.nextInt();
+		int subject = scan.nextInt();
 		
-		int s = scan.nextInt();
-		double[] a = new double[s];
-		for(int i=0;i<s;i++) {
-			a[i]=scan.nextDouble();
-		}
-	    double sum=0;
-		for(int i=0;i<s;i++) {
-			sum+=a[i];
-		}
-		double avg = sum/(double)s;
+		int[][] a = new int[student][subject];
 		
-		int count =0;
-		for(int i=0;i<s;i++) {
-			if(a[i]>avg) {
-				count++;
+		for(int i=0;i<student;i++) {
+			for(int j=0;j<subject;j++) {
+				a[i][j]=scan.nextInt();
 			}
 		}
-		double percentage = (count/(double)s)*100.0;
-		System.out.println("Total Employees:"+s);
-		System.out.println("Average Salary:"+avg);
-		System.out.println("Employees Above Average:"+count);
-		System.out.printf("Percentage: %.2f%% ",percentage);
+		System.out.println("Student Averages:");
+		for(int i=0;i<student;i++) {
+			int sum1=0;
+			for(int j=0;j<subject;j++) {
+				sum1+=a[i][j];
+			}
+			double avg = (double)sum1/(double)student;
+			System.out.printf("Student %d:%.2f\n",(i+1),avg);
+		}
+		System.out.println();
+		System.out.println("subjects averages:");
+		for(int j=0;j<subject;j++) {
+			int sum2=0;
+			for(int i=0;i<student;i++) {
+				sum2+=a[i][j];
+			}
+			double avg =(double)sum2/(double)subject;
+			System.out.printf("Subjects %d:%.2f\n",(j+1),avg);
+		}
 		
-		
-		
-
 	}
 
 }
