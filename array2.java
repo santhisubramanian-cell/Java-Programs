c71c0e60edd5375f83759ad92479846f86273dbe
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-07 19:46:49 +0530

    Add files via upload

--- array2.java
+++ array2.java
@@ -1,34 +1,53 @@
-package one_d_array;
-import java.util.Scanner;
+package two_d_array;
+import java.util.*;
 public class array2 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int n = scan.nextInt();
-		int threshold = scan.nextInt();
-		
-		int[] a = new int[n];
-		int count=0;
-		
-		String index="";
-		for(int i=0;i<n;i++) {
-			a[i]=scan.nextInt();
-		}
-		for(int i=0;i<n;i++) {
-			if(a[i]<threshold) {
-				if(count>0) {
-					index+=",";
-				}
-				index+=i;
-				count++;
-				
+		int row = scan.nextInt();
+		int col = scan.nextInt();
+		char[][] a = new char[row][col];
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				a[i][j]='A';
 			}
 		}
-		System.out.println("Total Products:"+n);
-		System.out.println("Minimum Threshold:"+threshold);
-		System.out.println("Low Stock Products:"+count);
-		System.out.println("Product Indices:["+index+"]");
+		
+		
+		int book = scan.nextInt();
+		
+		for(int i=1;i<=book;i++) {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			a[x][y]='B';
+		}
+		
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		int total = row*col;
+		int ava = book-total;
+		int rate = (book/total)*100;
+		
+		System.out.println("Total Seats:"+total);
+		System.out.println("Booked Seats:"+book);
+		System.out.println("Available Seats:"+ava);
+		System.out.println("Occupancy Rate:"+rate+"%");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
