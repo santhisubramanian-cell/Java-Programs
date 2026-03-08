e0e814a1a553a5fc9a141aca71f7a7331b940ed0
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-08 20:29:19 +0530

    Add files via upload

--- array9.java
+++ array9.java
@@ -1,42 +1,66 @@
-package one_d_array;
+package two_d_array;
 import java.util.Scanner;
 public class array9 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int n = scan.nextInt();
+		int row = scan.nextInt();
+		int col = scan.nextInt();
 		
-		double[] a = new double[n];
+		int[][] a = new int[row][col];
 		
-		for(int i=0;i<n;i++) {
-			a[i]=scan.nextDouble();
-		}
-		double max=a[0];
-		for(int i=0;i<n;i++) {
-			if(a[i]>max) {
-				max=a[i];
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				a[i][j]=scan.nextInt();
 			}
 		}
-		double min = max;
-		for(int i=0;i<n;i++) {
-			if(a[i]<min) {
-				min=a[i];
+		System.out.println("Elevation Map:");
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		int max = a[0][0];
+		
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				if(a[i][j]>max) {
+					max=a[i][j];
+				}
 			}
 		}
-		double sum =0;
-		for(int i=0;i<n;i++) {
-			sum+=a[i];
+		int min = max;
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				if(a[i][j]<min) {
+					min=a[i][j];
+				}
+			}
 		}
-		double avg = sum/(double)n;
-		System.out.println("Total Stores:"+n);
-		System.out.printf("Highest Sales:$%.2f",max);
-		System.out.println();
-		System.out.printf("Lowest Sales:$%.2f",min);
-		System.out.println();
-		System.out.printf("Total Sales:$%.2f",sum);
-		System.out.println();
-		System.out.printf("Average Sales:$%.2f",avg);
+		int range = max-min;
+		System.out.println("Highest Point:"+max+"m");
+		System.out.println("Lowest Point:"+min+"m");
+		System.out.println("Elevation Range:"+range+"m");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
