413b5b4328d9998292248b0b060a560780a06105
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-09 20:24:37 +0530

    Add files via upload

--- array24.java
+++ array24.java
@@ -1,58 +1,21 @@
-package one_d_array;
-
-import java.util.*;
-
+package two_d_array;
+import java.util.Scanner;
 public class array24 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			int num=1;
+			for(int j=0;j<=i;j++) {
+				System.out.print(num+" ");
+				num=num*(i-j)/(j+1);
+			}
+			System.out.println();
+		}
+	}
 
-        int n = sc.nextInt();
-        double[] value = new double[n];
-        for (int i = 0; i < n; i++) {
-            value[i] = sc.nextDouble();
-        }
-
-        double total = 0;
-        double max = value[0];
-        double min = value[0];
-        int maxIndex = 0;
-        int minIndex = 0;
-        for (int i = 0; i < n; i++) {
-            total += value[i];
-
-            if (value[i] > max) {
-                max = value[i];
-                maxIndex = i;
-            }
-
-            if (value[i] < min) {
-                min = value[i];
-                minIndex = i;
-            }
-        }
-
-        double average = total / n;
-        double bargainThreshold = average * 0.7;
-        double premiumThreshold = average * 1.5;
-
-        int bargainCount = 0;
-        int premiumCount = 0;
-        for (int i = 0; i < n; i++) {
-            if (value[i] < bargainThreshold) {
-                bargainCount++;
-            }
-            if (value[i] > premiumThreshold) {
-                premiumCount++;
-            }
-        }
-        System.out.println("Total Properties: " + n);
-        System.out.printf("Portfolio Value: $%.2f\n", total);
-        System.out.printf("Average Property Value: $%.2f\n", average);
-        System.out.printf("Most Valuable: $%.2f (Property %d)\n", max, maxIndex + 1);
-        System.out.printf("Least Valuable: $%.2f (Property %d)\n", min, minIndex + 1);
-        System.out.println("Bargain Properties (<70% avg): " + bargainCount);
-        System.out.println("Premium Properties (>150% avg): " + premiumCount);
-
-        
-    }
+	
 }
