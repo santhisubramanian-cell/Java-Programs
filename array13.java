4d7258e286cf723209bd05e9845d8174e89316cd
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-09 20:19:47 +0530

    Add files via upload

--- array13.java
+++ array13.java
@@ -1,63 +1,43 @@
-package one_d_array;
+package two_d_array;
 import java.util.Scanner;
 public class array13 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int n = scan.nextInt();
+		int row = scan.nextInt();
+		int col = scan.nextInt();
 		
-		int[] a = new int[n];
-		for(int i=0;i<n;i++) {
-			a[i]=scan.nextInt();
-		}
-		int sum=0;
-		int max=a[0];
-		for(int i=0;i<n;i++) {
-			sum+=a[i];
-			if(a[i]>max) {
-				max=a[i];
+		int[][] a = new int[row][col];
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				a[i][j]=scan.nextInt();
 			}
 		}
-		double avg = (double)sum/(double)n;
-		int ontime=0;
-		for(int i=0;i<n;i++) {
-			if(a[i]<=15) {
-				ontime++;
+		System.out.println("Seating Map:");
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				System.out.print(a[i][j]+" ");
 			}
+			System.out.println();
 		}
-		int compensation =0;
-		for(int i=0;i<n;i++) {
-			if(a[i]>180) {
-				compensation++;
+		int count=0;
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				if(a[i][j]==1) {
+					count++;
+				}
 			}
+			
 		}
-		double performance = ((double)ontime/(double)n)*100.0;
+		int total=row*col;
+		double rate = (double)count/(double)total*100.0;
+		System.out.println("Total Occupied:"+count);
+		System.out.printf("Occupancy Rate: %.2f%%",rate);
 		
-		System.out.println("Total Flights:"+n);
-		System.out.printf("Average Delay:%.2f",avg);
-		System.out.println();
-		System.out.println("Maximum Delay:"+max);
-		System.out.println("On-Time Flights:"+ontime);
-		System.out.println("Compensation Required:"+compensation);
-		System.out.printf("On-Time Performance:%.2f%%",performance);
+	
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
 	}
 
 }
