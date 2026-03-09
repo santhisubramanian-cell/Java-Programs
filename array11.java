7cae77121b9b82f4dcd739cb6f2676b1c36a5296
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-03-09 20:18:42 +0530

    Add files via upload

--- array11.java
+++ array11.java
@@ -1,50 +1,68 @@
-package one_d_array;
+package two_d_array;
+
 import java.util.Scanner;
+
 public class array11 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int n = scan.nextInt();
-		double[] a1 = new double[n];
-		double[] a2 = new double[n];
-		double[] a3 = new double[n];
-		
-		for(int i=0;i<n;i++) {
-			a1[i]=scan.nextDouble();
-		}
-		for(int i=0;i<n;i++) {
-			a2[i]=scan.nextDouble();
-		}
-		double sum=0;
-		for(int i=0;i<n;i++) {
-			a3[i]=a1[i]*a2[i];
-			sum+=a3[i];
-		}
-		int maxindex =-1;
-		double max = a3[0];
-		for(int i=0;i<n;i++) {
-			if(a3[i]>max) {
-				max=a3[i];
-				maxindex=i;
-			}
-		}
-		int minindex=-1;
-		double min=max;
-		for(int i=0;i<n;i++) {
-			if(a3[i]<min) {
-				min=a3[i];
-				minindex=i;
-			}
-		}
-		System.out.println("Number of Assets:"+n);
-		System.out.printf("Total Portfolio Value:%.2f",sum);
-		System.out.println();
-		System.out.printf("Most Valuable Asset:Asset %.2f",max,maxindex);
-		System.out.println();
-		System.out.printf("Least Valuable Asset:Asset %.2f",min,minindex);
-		
-		
-	}
+        Scanner sc = new Scanner(System.in);
 
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] seat = new int[r][c];
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                seat[i][j] = sc.nextInt();
+            }
+        }
+
+        int violations = 0;
+
+        System.out.println("Seating Layout:");
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                System.out.print(seat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nViolation Coordinates:");
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+
+                if(seat[i][j] == 1) {
+
+                    for(int x = -1; x <= 1; x++) {
+                        for(int y = -1; y <= 1; y++) {
+
+                            if(x == 0 && y == 0) continue;
+
+                            int ni = i + x;
+                            int nj = j + y;
+
+                            if(ni >= 0 && ni < r && nj >= 0 && nj < c) {
+                                if(seat[ni][nj] == 1) {
+                                    System.out.println("- Seat at ("+i+","+j+") is too close to ("+ni+","+nj+")");
+                                    violations++;
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        System.out.println("\nViolations Found: " + violations);
+
+        if(violations == 0)
+            System.out.println("Status: Compliant");
+        else
+            System.out.println("Status: Non-Compliant");
+    }
 }
